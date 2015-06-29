789705a63639b9fce6f29bfe55afec92f5236a83
David T. Sadler <davidtsadler@googlemail.com>
2015-06-29 13:32:47 +0100

    0.1.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # CHANGELOG
  
-## Unreleased
+## 0.1.0 - 2015-06-29
 
 ### Initial release
--- src/DTS/eBaySDK/ResolutionCaseManagement/Services/ResolutionCaseManagementBaseService.php
+++ src/DTS/eBaySDK/ResolutionCaseManagement/Services/ResolutionCaseManagementBaseService.php
@@ -25,7 +25,7 @@ class ResolutionCaseManagementBaseService extends \DTS\eBaySDK\Services\BaseServ
     /**
      * @var string Current version of the SDK
      */
-    const VERSION = '0.0.0';
+    const VERSION = '0.1.0';
 
     /**
      * Constants for the various HTTP headers required by the API.
